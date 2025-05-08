1be8998bad90f395abb89e07b588886b5eeb7acf
rogi52 <keisuke.korogi.52@gmail.com>
2025-05-09 05:47:49 +0900

    b

--- graph/bellman_ford.hpp
+++ graph/bellman_ford.hpp
@@ -1,14 +1,13 @@
 #include "template.hpp"
 
-template < class T > pair<bool, vector< T >> bellman_ford(int n, const vector<tuple<int, int, T>>& es, int s) {
+template < class T > vector< T > bellman_ford(int n, const vector<tuple<int, int, T>>& es, int s) {
     const T INF = numeric_limits< T >::max();
     vector< T > d(n, INF); d[s] = 0;
-    while(n--) {
-        bool updated = false;
-        for(const auto &[u, v, c] : es) if(d[u] != INF) {
-            if(chmin(d[v], d[u] + c)) updated = true;
+    FOR(n - 1) {
+        for(auto [u, v, c] : es) {
+            if(d[u] != INF) chmin(d[v], d[u] + c);
         }
-        if(not updated) break;
     }
-    return pair{n == 0, d};
+    for(auto [u, v, c] : es) if(d[u] != INF and chmin(d[v], d[u] + c)) return {};
+    return d;
 }
