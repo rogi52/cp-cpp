be0850e1d771cb03224325c4f7b8fea80598d5ff
rogi52 <keisuke.korogi.52@gmail.com>
2025-09-10 19:26:02 +0900

    mat product

--- mat/prod.hpp
+++ mat/prod.hpp
@@ -9,4 +9,12 @@ template < class T > vector<vector< T >> prod(const vector<vector< T >>& A, cons
     FOR(m, M) FOR(n, N) FOR(k, K) C[n][k] += A[n][m] * B[m][k];
     return C;
 }
+// A^n
+template < class T > vector<vector< T >> pow(vector<vector< T >> A, u64 n) {
+    const int N = ssize(A);
+    vector ans(N, vector(N, T(0)));
+    FOR(i, N) ans[i][i] = 1;
+    for(; n; n >>= 1) { if(n & 1) ans = prod(ans, A); A = prod(A, A); }
+    return ans;
+}
 }
