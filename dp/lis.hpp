a8abc8c9286a48a3e6f353cf1960b2b5b865ee46
rogi52 <keisuke.korogi.52@gmail.com>
2025-10-30 09:39:45 +0900

    lis, rerooting

--- dp/lis.hpp
+++ dp/lis.hpp
@@ -1,19 +1,28 @@
 #include "template.hpp"
 
+/*
+LISの長さ: m
+idx = {i1, ..., im}
+a[i1] < ... < a[im]
+*/
 template < class T, class Cmp >
-tuple< vector< T >, vector<int>, vector<int> > LIS(const vector< T >& a, Cmp cmp) {
-    int n = a.size();
-    vector< T > l_s;
+pair< vector<int>, vector<int> > LIS(const vector< T >& a, const Cmp& cmp) {
+    const int n = ssize(a);
+    vector< T > dp;
     vector<int> st(n, -1), nt(n, -1), rank(n);
     FOR(i, n) {
-        int pos = lower_bound(l_s.begin(), l_s.end(), a[i], cmp) - l_s.begin();
-        st[pos] = i;
-        if(pos >= 1) nt[i] = st[pos - 1];
-        if(pos == int(l_s.size())) l_s.push_back(a[i]); else l_s[pos] = a[i];
-        rank[i] = pos + 1;
+        const int p = lower_bound(dp.begin(), dp.end(), a[i], cmp) - dp.begin();
+        st[p] = i;
+        if(1 <= p) nt[i] = st[p - 1];
+        if(p == ssize(dp)) {
+            dp.push_back(a[i]);
+        } else {
+            dp[p] = a[i];
+        }
+        rank[i] = p + 1;
     }
-    int len = l_s.size();
-    vector<int> idx(len);
-    for(int i = st[len - 1]; i >= 0; i = nt[i]) idx[--len] = i;
-    return {l_s, idx, rank};
+    int m = ssize(dp);
+    vector<int> idx(m);
+    for(int i = st[m - 1]; i >= 0; i = nt[i]) idx[--m] = i;
+    return {idx, rank};
 }
---
+++ tree/rerooting.hpp
@@ -0,0 +1,80 @@
+#include "template.hpp"
+#include "tree/hld.hpp"
+
+/*
+edge_type: {to, e_id}
+*/
+template < class T, class EE, class EV, class VP, class ID > struct rerooting {
+    int n;
+    int r;
+    vector< T > dp, dp_r, ans;
+    vector<vector< T >> sub;
+    vector<vector<pair<int, int>>> tree;
+    EE f_ee;
+    EV f_ev;
+    VP f_vp;
+    ID id;
+    rerooting(
+        const vector<vector<pair<int, int>>>& tree,
+        const EE& f_ee,
+        const EV& f_ev,
+        const VP& f_vp,
+        const ID& id
+    ) : n(ssize(tree)), tree(tree), f_ee(f_ee), f_ev(f_ev), f_vp(f_vp), id(id) {}
+
+    void solve(int r) {
+        this->r = r;
+        dp.assign(n, id());
+        sub.resize(n);
+        FOR(v, n) sub[v].resize(ssize(tree[v]));
+        auto dfs = [&](auto&& dfs, int v, int p) -> T {
+            FOR(i, ssize(tree[v])) {
+                auto [to, e_id] = tree[v][i];
+                if(to != p) {
+                    sub[v][i] = dfs(dfs, to, v);
+                    dp[v] = f_ee(dp[v], f_ev(sub[v][i], e_id));
+                }
+            }
+            return dp[v] = f_vp(dp[v], v);
+        }; dfs(dfs, r, -1);
+    }
+
+    void reroot() {
+        dp_r.assign(n, id());
+        auto dfs = [&](auto&& dfs, int v, int p, T s) -> void {
+            const int m = ssize(tree[v]);
+            FOR(i, m) {
+                auto [to, e_id] = tree[v][i];
+                if(to == p) sub[v][i] = s;
+            }
+            vector< T > R(m + 1);
+            R[m] = id();
+            REV(i, m) {
+                auto [to, e_id] = tree[v][i];
+                R[i] = f_ee(R[i + 1], f_ev(sub[v][i], e_id));
+            }
+            T L = id();
+            FOR(i, m) {
+                auto [to, e_id] = tree[v][i];
+                if(to != p) dfs(dfs, to, v, f_vp(f_ee(L, R[i + 1]), v));
+                dp_r[to] = f_vp(f_ee(L, R[i + 1]), v);
+                L = f_ee(L, f_ev(sub[v][i], e_id));
+            }
+        }; dfs(dfs, r, -1, id());
+
+        ans.assign(n, id());
+        FOR(v, n) {
+            FOR(i, ssize(tree[v])) {
+                auto [to, e_id] = tree[v][i];
+                ans[v] = f_ee(ans[v], f_ev(sub[v][i], e_id));
+            }
+            ans[v] = f_vp(ans[v], v);
+        }
+    }
+
+    T get(int r, int v, treeHLD& hld) {
+        if(r == v) return ans[v];
+        if(not hld.in_subtree(v, r)) return dp[v] ;
+        return dp_r[hld.jump(v, r, 1)];
+    }
+};
